75734a3cd12aded922e1ccfdcf5da4ab14d3ee9d
xiaochenwang94 <xiaochenwang94@gmail.com>
2017-03-28 13:58:29 +0800

    new dataset

--- code/main.py
+++ code/main.py
@@ -7,8 +7,11 @@ import numpy as np
 from collections import defaultdict
 import matplotlib.pyplot as plt
 
-madison_long = -73.993278
-madison_lati = 40.750456
+# Yankee Stadium 40.829602, -73.926099
+# madioson 40.750456 -73.993278
+# STAPLES Center 34.043124, -118.267254
+madison_long = -73.84640277
+madison_lati = 40.75549922
 
 
 class Result(object):
@@ -42,8 +45,8 @@ def process_foursquare():
     data = pd.read_csv('../data/madison.csv')
     print('Processing foursquare data......')
     data['date'] = pd.to_datetime(data['date'], errors='coerce')
-    tmp = data[data['date'] < '2010-03-10']
-    tmp = tmp[tmp['date'] > '2010-03-01']
+    tmp = data[data['date'] < '2016-04-20']
+    tmp = tmp[tmp['date'] > '2016-04-14']
     # print(data['date'])
     tmp.to_csv('../data/processed_madison.csv')
     print('4sq data processed.')
@@ -79,7 +82,7 @@ def score(word, locations):
         ax.plot(selx, sely, 'r.', markersize=2)
         ax.set_xlim([xmin, xmax])
         ax.set_ylim([ymin, ymax])
-        print(word, kernel((0,0)))
+        # print(word, kernel((0,0)))
         plt.show()
     return Result(word,kernel((0,0)))
 
@@ -88,7 +91,8 @@ def score(word, locations):
 def kde(data, check, k=5):
     print('Using kernel density estimation......')
     # 选出check当天的所有数据
-    select = data[data['time'] == check.date]
+    # select = data[data['Date'] == check.date]
+    select = data[data['Date'] == '2016-04-16']
     print('date: %s \ndataset shape %s' % (check.date, select.shape[0]))
     # 总结出当天的word list
     word_list = defaultdict(list)
@@ -96,12 +100,12 @@ def kde(data, check, k=5):
                     'it','but','got','my','and','of','so','up','was','all',
                     'that','this','you','he','she','like','for']
     for index, row in select.iterrows():
-        words = set(row['tweets'].split())
+        words = set(row['Tweet content'].split())
         for w in words:
             w = w.lower()
             if w in filter_words:
                 continue
-            word_list[w].append((row['lati'], row['long']))
+            word_list[w].append((row['Latitude'], row['Longitude']))
     # 计算每个单词的概率,排序
     result = []
     print('word list size is %s' % word_list.__len__())
@@ -126,40 +130,37 @@ def annotate(tweets, checkins):
     ret = {}
     for index, row in checkins.iterrows():
         words = kde(tweets, row)
-    # for idx in range(checkins.shape[0]):
-    #     words = kde(tweets, checkins.iloc[idx])
+        a = input('next word\n')
     print('annotation ended......')
     return ret
 
 def initialize_data(fsq_file, tweets_file):
     if not os.path.isfile(tweets_file):
-        data = pd.read_csv('../data/tweets.csv')
+        data = pd.read_csv('../data/tweets.csv',encoding = 'ISO-8859-1')
+        # 34.049770, -118.238735
         idx = data['Latitude'] <= 42
         idx &= data['Latitude'] >= 38
         idx &= data['Longitude'] <= -72
         idx &= data['Longitude'] >= -76
         data = data[idx]
-        # data = data.loc[:,['Tweet Id', 'Latitude', 'Longitude',
-        #                    'Tweet content','Date']]
+        data = data.loc[:,['Tweet Id', 'Latitude', 'Longitude',
+                           'Tweet content','Date']]
         data = data.set_index('Tweet Id', inplace=False, drop=True)
         data.to_csv(tweets_file)
+    if not os.path.isfile(fsq_file):
+        process_foursquare()
 
 
 if __name__ == '__main__':
 
-    fsq_file = '../data/fsq.csv'
+    fsq_file = '../data/processed_madison.csv'
     tweets_file = '../data/tweets_processed.csv'
     initialize_data(fsq_file, tweets_file)
 
-    # if not os.path.isfile('../data/processed_data.csv'):
-    #     process_data()
-    # if not os.path.isfile('../data/processed_madison.csv'):
-    #     process_foursquare()
-    #
-    # tweets = pd.read_csv('../data/processed_data.csv')
-    # fsq = pd.read_csv('../data/processed_madison.csv')
-    # annotation = annotate(tweets, fsq)
-    # print('end......')
+    tweets = pd.read_csv('../data/tweets_processed.csv')
+    fsq = pd.read_csv('../data/processed_madison.csv')
+    annotation = annotate(tweets, fsq)
+    print('end......')
 
 
 
